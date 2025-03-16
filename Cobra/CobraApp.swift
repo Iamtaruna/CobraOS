fd7ca16daf264bfa3b66b7fb08121b667fc0e9e0
Taruna Singh <tarunax3@gmail.com>
2025-03-16 15:11:24 -0400

    Initial Commit

---
+++ Cobra/CobraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobraApp.swift
+//  Cobra
+//
+//  Created by Taruna Singh on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CobraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
